e3983ae20c26548effe5e7bf76f5892c866a91fa
mcrbilly7 <nossonkosowsky@gmail.com>
2025-07-27 16:35:43 -0500

    Create Terms.js

---
+++ src/pages/Terms.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Terms = () => {
+  return (
+    <div className="container">
+      <h1>Terms of Service</h1>
+      <p>You must agree to our terms before creating an account. Updated policies are available in your dashboard.</p>
+    </div>
+  );
+};
+
+export default Terms;
