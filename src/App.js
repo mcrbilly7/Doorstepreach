288ac1ca8952b741d23f9c2614130b2299068570
mcrbilly7 <nossonkosowsky@gmail.com>
2025-07-28 02:12:40 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
 import Stepper from './pages/Stepper';
 import Business from './pages/Business';
-import About from './pages/About';
-import Terms from './pages/Terms';
-import NotFound from './pages/NotFound';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
         <Route path="/stepper" element={<Stepper />} />
         <Route path="/business" element={<Business />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="*" element={<NotFound />} />
+        {/* Add more routes here */}
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
